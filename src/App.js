280b54f257a7f7d4f8130f427b5fbbd93e2fe3af
trodriguez89 <trodriguez89@gmail.com>
2019-12-09 12:44:06 -0800

    mvp wip, ProductContext and CartContext created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from "./contexts/ProductContext";
-import { CartContext} from "./contexts/CartContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -21,10 +21,12 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
-				<Route path="/cart" component={ShoppingCart} />
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+					<Route path="/cart" component={ShoppingCart} />
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
